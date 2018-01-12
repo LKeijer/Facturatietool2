b9e48b25d6cb3778ee30c204d2172e275915a742
Roy Smits <33250998+RoySmits@users.noreply.github.com>
2018-01-12 18:37:29 +0100

    UI optimalisation
    Hoofd menu verandert van ComboBox naar Button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,21 @@ namespace Offerte_en_Facturatie_UI
         public MainWindow()
         {
             InitializeComponent();
+          
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).ContextMenu.IsEnabled = true;
+            (sender as Button).ContextMenu.PlacementTarget = (sender as Button);
+            (sender as Button).ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+            (sender as Button).ContextMenu.IsOpen = true;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
