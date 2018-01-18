e8fa8057684b61c56870fdaaa943ad91bbcd3d7a
Wafflebasket <luke.keijer@mail.com>
2018-01-18 15:29:12 +0100

    1

---
+++ Klant.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Offerte_en_Facturatie_UI
+{
+    public class Klant
+    {
+
+
+        public void InsertKlant()
+        {
+            MainWindow main = new MainWindow(); // deze moet weg
+            string bedrijfNaam = main.BedrijfNaam;
+            string kvkNummer = main.KvK;
+            string btwNummer = main.BTW;
+            string voorNaam = main.Voornaam;
+            string achterNaam = main.Achternaam;
+            string straatNaam = main.Straatnaam;
+            string huisNummer = main.Huisnummer;
+            string postcode = main.Postcode;
+            string plaats = main.Plaats;
+            string telefoon = main.Telefoon;
+            string email = main.Email;
+            string website = main.Website;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+            SqlConnection myConnection = new SqlConnection(connectionString);
+            SqlDataAdapter command = new SqlDataAdapter();
+
+            command.InsertCommand = new SqlCommand("INSERT INTO Bedrijf (Bedrijfsnaam, Email, Website, KvKnummer, BTWnummer) VALUES (@bedrijfNaam, @email, @website, @kvknummer,@btwnummer)", myConnection);
+            command.InsertCommand.Parameters.Add("@bedrijfNaam", SqlDbType.VarChar).Value = bedrijfNaam;
+            command.InsertCommand.Parameters.Add("@kvknummer", SqlDbType.VarChar).Value = kvkNummer;//
+            command.InsertCommand.Parameters.Add("@btwnummer", SqlDbType.VarChar).Value = btwNummer;//
+            command.InsertCommand.Parameters.Add("@website", SqlDbType.VarChar).Value = website;//
+            command.InsertCommand.Parameters.Add("@email", SqlDbType.VarChar).Value = email;//
+
+            try //inserts data into bedrijf table
+            {
+                myConnection.Open();
+                command.InsertCommand.ExecuteNonQuery();
+                MessageBox.Show("inserted into bedrijf " + bedrijfNaam + email + website + kvkNummer + btwNummer);
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+
+            command.InsertCommand = new SqlCommand("INSERT INTO Particulier (Email, Telefoonnummer) VALUES (@email, @telnummer)", myConnection);
+            command.InsertCommand.Parameters.Add("@telnummer", SqlDbType.VarChar).Value = telefoon;//
+            command.InsertCommand.Parameters.Add("@email", SqlDbType.VarChar).Value = email;//
+
+            try // inserts data into particulier table
+            {
+                myConnection.Open();
+                command.InsertCommand.ExecuteNonQuery();
+                MessageBox.Show("inserted into particulier" + email + telefoon);
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            command.InsertCommand = new SqlCommand("INSERT INTO Persoon (Voornaam, Achternaam) VALUES (@voornaam, @achternaam)", myConnection);
+            command.InsertCommand.Parameters.Add("@voornaam", SqlDbType.VarChar).Value = voorNaam;
+            command.InsertCommand.Parameters.Add("@achternaam", SqlDbType.VarChar).Value = achterNaam;
+            try // inserts data into table persoon.
+            {
+                myConnection.Open();
+                command.InsertCommand.ExecuteNonQuery();
+                MessageBox.Show("inserted into Persoon " + voorNaam + achterNaam);
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+
+            command.InsertCommand = new SqlCommand("INSERT INTO Adres (Straatnaam, Huisnummer, Postcode, Plaatsnaam) VALUES (@straatnaam, @huisnummer, @postcode, @plaats)", myConnection);
+            command.InsertCommand.Parameters.Add("@straatnaam", SqlDbType.VarChar).Value = straatNaam;
+            command.InsertCommand.Parameters.Add("@huisnummer", SqlDbType.VarChar).Value = huisNummer;
+            command.InsertCommand.Parameters.Add("@postcode", SqlDbType.VarChar).Value = postcode;
+            command.InsertCommand.Parameters.Add("@plaats", SqlDbType.VarChar).Value = plaats;
+            try
+            {
+                myConnection.Open();
+                command.InsertCommand.ExecuteNonQuery();
+                MessageBox.Show("inserted into Adres " + straatNaam + huisNummer + postcode + plaats);
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+
+        }
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,11 +23,49 @@ namespace Offerte_en_Facturatie_UI
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public MainWindow(string bedrijf)
+        {
+            bedrijfInput = bedrijf;
+        }
+
+        string bedrijfInput;
+        string kvkInput;
+        string btwNummer;
+        string voornaam;
+        string achternaam;
+        string straatnaam;
+        string huisnummer;
+        string postcode;
+        string plaats;
+        string telefoonnummer;
+        string email;
+        string website;
+
+        Klant klant = new Klant();
+
         public MainWindow()
         {
             InitializeComponent();
-          
+
         }
+
+        public void SetTextBoxes()
+        {
+            bedrijfInput = Form1_Bedrijfsnaam.Text;
+            kvkInput = Form1_KvKNummer.Text;
+            btwNummer = Form1_BTWNummer.Text;
+            voornaam = Form1_ContactVoornaam.Text;
+            achternaam = Form1_Achternaam.Text;
+            huisnummer = Form1_Huisnummer.Text;
+            postcode = Form1_Postcode.Text;
+            plaats = Form1_Plaats.Text;
+            telefoonnummer = Form1_TelNummer.Text;
+            email = Form1_EMail.Text;
+            website = Form1_Website.Text;
+        }
+
+        #region WPF Controls
 
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
@@ -34,7 +75,7 @@ namespace Offerte_en_Facturatie_UI
             (sender as Button).ContextMenu.IsOpen = true;
         }
 
-        
+
         private void BtnNieuweKlant_Click(object sender, RoutedEventArgs e)
         {
             klanttabControl.SelectedItem = tab_NieuweKlant;
@@ -87,6 +128,83 @@ namespace Offerte_en_Facturatie_UI
         {
             factuurtabControl.SelectedItem = tab_FactuurBekijken;
         }
+        #endregion
 
+        #region GET/SET TEXTBOX STRINGS
+        public string BedrijfNaam
+        {
+            get { return bedrijfInput; }
+            set { bedrijfInput = value; }
+        }
+        public string KvK
+        {
+            get { return kvkInput; }
+            set { kvkInput = value; }
+        }
+        public string BTW
+        {
+            get { return btwNummer; }
+            set { btwNummer = value; }
+        }
+
+        public string Voornaam
+        {
+            get { return voornaam; }
+            set { voornaam = value; }
+        }
+
+        public string Achternaam
+        {
+            get { return achternaam; }
+            set { achternaam = value;}
+        }
+
+        public string Straatnaam
+        {
+            get { return straatnaam; }
+            set { straatnaam = value; }
+        }
+
+        public string Huisnummer
+        {
+            get { return huisnummer; }
+            set { huisnummer = value; }
+        }
+
+        public string Postcode
+        {
+            get { return postcode; }
+            set { postcode = value; }
+        }
+
+        public string Plaats
+        {
+            get { return plaats; }
+            set { plaats = value; }
+        }
+
+        public string Telefoon
+        {
+            get { return telefoonnummer; }
+            set { telefoonnummer = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Website
+        {
+            get { return website; }
+            set { website = value; }
+        }
+#endregion
+
+        private void Form1_btnAccepteer_Click(object sender, RoutedEventArgs e)
+        {
+            SetTextBoxes();
+            klant.InsertKlant();
+        }
     }
 }
